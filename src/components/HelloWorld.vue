77fb9ab2c0356d9598b0a5d52fa169d0432abb4d
gzh <gzh>
2018-03-13 17:35:49 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <appHeader></appHeader>
+    <div class="a">bb</div>
+  </div>
+</template>
+
+<script>
+import AppHeader  from './test'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    components:{
+      AppHeader 
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.a{color: springgreen}
+</style>
